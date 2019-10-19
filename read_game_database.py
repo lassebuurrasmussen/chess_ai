3a537f156656657dc8d65ecf2bd3d803218def5d
lr <wogie636@gmail.com>
2019-10-19 18:29:00 +0200

    made function to extract numpy array states from PGN file

--- read_game_database.py
+++ read_game_database.py
@@ -1,23 +1,35 @@
-from chess import pgn
+import numpy as np
+from chess import pgn, Board
 from tqdm import tqdm
-import joblib
+
+from utility_module import board_2_array, count_games_from_pgn
 
 GAMES_TO_READ = 10_000
 INPUT_FILE = "./game_data/KingBase2019-A00-A39.pgn"
-OUTPUT_FILE = INPUT_FILE.replace('.pgn', f'_{GAMES_TO_READ}_games.dump')
 
-pgn_file = open(
-    INPUT_FILE,
-    encoding="utf-8-sig")  # Encoding -> https://python-chess.readthedocs.io/en/latest/pgn.html
 
-games_moves = []
-for game_i in tqdm(range(GAMES_TO_READ)):
-    game = pgn.read_game(pgn_file)
+def get_states_from_pgn(input_file, games_to_get=None):
+    # Encoding -> https://python-chess.readthedocs.io/en/latest/pgn.html
+    n_games = count_games_from_pgn(input_file=input_file)
+    pgn_file = open(input_file, encoding="utf-8-sig")
 
-    try:
-        games_moves.append([move.uci() for move in game.mainline_moves()])
-    except AttributeError:
-        print(f"Done at game {game_i}")
-        break
+    if games_to_get is None:
+        games_to_get = n_games
 
-joblib.dump(games_moves, OUTPUT_FILE)
+    states = []
+    # for game_i in range(GAMES_TO_READ)[0:10]:
+    for game_i in tqdm(range(games_to_get)):
+
+        game = pgn.read_game(pgn_file)
+        assert not game.errors
+
+        board = Board()
+        for move in game.mainline_moves():
+            state = board_2_array(board)
+            states.append(state)
+
+            board.push(move)
+
+        state = board_2_array(board)
+        states.append(state)
+    states = np.array(states)
