94ce783e8ba545741380100e854eb4664d78c182
lr <wogie636@gmail.com>
2019-11-14 16:22:56 +0100

    Use resnet18 instead

--- neural_nets.py
+++ neural_nets.py
@@ -4,51 +4,16 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 # noinspection PyPep8Naming
-import torch.nn.functional as F
 from tqdm import tqdm
 
 import utility_module as ut
-
-
-class ReluConv(nn.Module):
-    """A convolution with relu activation"""
-
-    def __init__(self, in_channels, out_channels, kernel_size, stride, padding):
-        super(ReluConv, self).__init__()
-
-        self.conv = nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
-                              kernel_size=kernel_size, stride=stride, padding=padding)
-        self.maxpooling = nn.MaxPool2d(3, padding=padding)
-
-    def forward(self, x):
-        return F.relu(self.conv(x))
-
-
-class LegalMovePredictor(nn.Module):
-    def __init__(self):
-        super(LegalMovePredictor, self).__init__()
-        out_channels = 3
-        self.conv1 = ReluConv(in_channels=12, out_channels=out_channels, kernel_size=3, stride=1,
-                              padding=1)
-
-        n_moves = 4032
-        self.dense = nn.Linear(out_channels * 8 * 8, 1024)
-        self.classification = nn.Linear(1024, n_moves)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        downsample = F.dropout(self.conv1(x))
-
-        flat = downsample.view(downsample.shape[0], -1)
-        dense_out = F.dropout(self.dense(flat))
-        output = F.softmax(F.dropout(self.classification(dense_out)), 1)
-
-        return output
+from resnet_module import ResNet, BasicBlock
 
 
 def run_epoch(batch_x: torch.Tensor, batch_y: torch.Tensor, optimizer, criterion, net, batch_size,
               losses, time_steps):
     start_idxs = range(0, batch_x.shape[0], batch_size)
-    for minibatch_i, start_idx in enumerate(start_idxs):
+    for minibatch_i, start_idx in tqdm(list(enumerate(start_idxs))):
         optimizer.zero_grad()
 
         slicer = slice(start_idx, start_idx + batch_size)
@@ -79,27 +44,27 @@ def evaluate(in_net, in_val_x, in_val_y, criterion, val_losses, time_steps):
 
 
 def fit(batch_x, batch_y, batch_size, n_epochs, lr):
-    lmp: LegalMovePredictor = LegalMovePredictor()
-    lmp.float()
-    optimizer: torch.optim.Adam = torch.optim.Adam(lmp.parameters(), lr=lr)
+    model: ResNet = ResNet(block=BasicBlock, layers=[2, 2, 2, 2], num_classes=4032)
+    model.float()
+    optimizer: torch.optim.Adam = torch.optim.Adam(model.parameters(), lr=lr)
     criterion = nn.CrossEntropyLoss()
 
     losses = []
     val_losses = {}
     time_steps = [0]
-    for _ in tqdm(range(n_epochs)):
-        run_epoch(batch_x, batch_y, optimizer=optimizer, criterion=criterion, net=lmp,
+    for _ in range(n_epochs):
+        run_epoch(batch_x, batch_y, optimizer=optimizer, criterion=criterion, net=model,
                   batch_size=batch_size, losses=losses, time_steps=time_steps)
 
         # if not epoch % 10:
-        evaluate(in_net=lmp, in_val_x=val_X, in_val_y=val_y, criterion=criterion,
+        evaluate(in_net=model, in_val_x=val_X, in_val_y=val_y, criterion=criterion,
                  val_losses=val_losses, time_steps=time_steps)
 
-    return lmp, losses, time_steps, val_losses
+    return model, losses, time_steps, val_losses
 
 
-dp_slicer = slice(14_000)  # N data points to use
-N_EPOCHS = 3
+dp_slicer = slice(10_000)  # N data points to use
+N_EPOCHS = 2
 LR = 1e-3
 BATCH_SIZE = 128
 
@@ -120,14 +85,25 @@ plt.plot(ts[1:], train_losses)
 plt.plot(*list(zip(*val_l.items())))
 plt.show()
 
-# Evaluate in-sample accuracy and correctness
+# Evaluate accuracy and correctness
 preds = legal_moves_net(X).argmax(1)
 accuracy = (preds == y).int().float().mean()
+
+preds_val = legal_moves_net(val_X).argmax(1)
+accuracy_val = (preds_val == val_y).int().float().mean()
 
 correct = 0
 for i, fen in enumerate(fens):
     if preds[i] in [ut.uci2onehot_idx(str(uci)) for uci in chess.Board(fen).legal_moves]:
         correct += 1
 
+correct_val = 0
+for i, fen in enumerate(val_fens):
+    if preds_val[i] in [ut.uci2onehot_idx(str(uci)) for uci in chess.Board(fen).legal_moves]:
+        correct_val += 1
+
 print(f"Train accuracy: {accuracy:.3f}")
 print(f"Train prediction correct ratio: {correct / len(fens):.3f}")
+
+print(f"Test accuracy: {accuracy_val:.3f}")
+print(f"Test prediction correct ratio: {correct_val / len(val_fens):.3f}")
